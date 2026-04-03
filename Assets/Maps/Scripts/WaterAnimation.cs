415f56bc5f513ca31f4d55ace1a525c923d0728e
Mohmad&W <mohmadwahbi22@gmail.com>
2026-04-03 11:26:55 +0200

    Fix Map version1

---
+++ Assets/Maps/Scripts/WaterAnimation.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class WaterAnimation : MonoBehaviour
+{
+    [ExecuteAlways]
+    public class WaterManager : MonoBehaviour
+    {
+        public Material waterMaterial;
+        public float speed = 0.05f;
+        void Update()
+        {
+            float offsetX = Time.time * speed;
+            float offsetY = Time.time * speed * 0.3f;
+            waterMaterial.SetTextureOffset("_BaseMap", new Vector2(offsetX, offsetY));
+        }
+    }
+}
