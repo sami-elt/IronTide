cf4f7b70f94fc2009449bbbebaf80ff22099acf6
Mohmad&W <mohmadwahbi22@gmail.com>
2026-05-13 00:28:14 +0200

    Add tutorial + Fix GameDemo scen

--- Assets/SlimUI/Modern Menu 1/Scripts/Managers/UIMenuManager.cs
+++ Assets/SlimUI/Modern Menu 1/Scripts/Managers/UIMenuManager.cs
@@ -23,7 +23,7 @@ namespace SlimUI.ModernMenu{
         public GameObject playerSetupCanvas;
         public GameObject setupWindow;
         public PlayerSetupUI playerSetupUI;
-
+        public GameObject tutorialPanel;
         public enum Theme {custom1, custom2, custom3};
         [Header("THEME SETTINGS")]
         public Theme theme;
@@ -278,6 +278,16 @@ namespace SlimUI.ModernMenu{
 
             // 🔥 Visa rätt antal slots
             playerSetupUI.ShowPlayers(playerCount);
+        }
+
+        public void OpenTutorial()
+        {
+            tutorialPanel.SetActive(true);
+        }
+
+        public void CloseTutorial()
+        {
+            tutorialPanel.SetActive(false);
         }
         // Load Bar synching animation
         IEnumerator LoadAsynchronously(string sceneName){ // scene name is just the name of the current scene being loaded
